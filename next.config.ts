e2f77a199c259007104b1e6d0fe8463451677b85
Esperanza <xsq230106@gmail.com>
2026-07-12 17:37:04 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/Siqi-Xu-AI-Product-Portfolio",
   output: process.env.NODE_ENV === "production" ? "export" : undefined,
 };
 
